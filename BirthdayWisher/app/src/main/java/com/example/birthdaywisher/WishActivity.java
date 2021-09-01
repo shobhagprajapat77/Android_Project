6dbc9bbb24ca35d4f602c814628df67a89b2b625
shobhagprajapat77 <shobhagprajapat77@users.noreply.github.com>
2021-09-01 10:55:30 +0530

    First version of tictoetac gam

---
+++ BirthdayWisher/app/src/main/java/com/example/birthdaywisher/WishActivity.java
@@ -0,0 +1,14 @@
+package com.example.birthdaywisher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wish);
+    }
+}
